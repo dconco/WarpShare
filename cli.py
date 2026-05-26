a714154bf3684bf545a94e40ef34a8fbf94b769b
dconco <onipededavid208@gmail.com>
2026-05-26 10:30:06 +0000

    updated

--- cli.py
+++ cli.py
@@ -1,9 +1,10 @@
 import os
 import typer
-from rich.console import Console
-from utils.network_ip import get_local_ip
 from server import create_app
 from waitress import serve
+from rich.console import Console
+from helpers.tunnel import start_tunnel
+from utils.network_ip import get_local_ip
 
 # Initialize Typer and Rich console for clean formatting
 cli = typer.Typer(
@@ -19,11 +20,6 @@ def start_server(host: str, port: int):
    app = create_app()
    serve(app, host=host, port=port)
 
-def tunnel(port):
-   import subprocess
-   subprocess.run(["cloudflared", "tunnel", "--url", f"http://127.0.0.1:{port}"])
-
-
 # LOCAL SHARE
 
 @cli.command(name="local")
@@ -35,13 +31,13 @@ def local_share(
    Share files instantly with anyone on your local Wi-Fi network.
    """
    if not os.path.exists(path):
-      console.print(f"[bold red]Error:[/bold red] Path '{path}' does not exist.")
+      console.print(f"[bold red][Error:][/bold red] Path '{path}' does not exist.")
       raise typer.Exit(code=1)
 
    ip = get_local_ip()
 
    if ip is None:
-      console.print(f"[bold red]Error:[/bold red] No Network IP found. Make sure you're connected to the same network with the receiver.")
+      console.print(f"[bold red][Error:][/bold red] No Network IP found. Make sure you're connected to the same network with the receiver.")
       raise typer.Exit(code=1)
 
    console.print(f"[bold green]Local Sharing Server started at http://{ip}:{port}[/bold green]")
@@ -55,7 +51,7 @@ def local_share(
 @cli.command(name="remote")
 def remote_share(
    path: str = typer.Argument(os.getcwd(), help="The directory path or file to share"),
-   port: int = typer.Option(8080, "--port", "-p", help="Internal port for the tunnel"),
+   port: int = typer.Option(3301, "--port", "-p", help="Internal port for the tunnel"),
 ):
    import threading
 
@@ -63,7 +59,7 @@ def remote_share(
    Share files over the public internet with a remote receiver.
    """
    if not os.path.exists(path):
-      console.print(f"[bold red]Error:[/bold red] Path '{path}' does not exist.")
+      console.print(f"[bold red][Error:][/bold red] Path '{path}' does not exist.")
       raise typer.Exit(code=1)
 
    console.print(f"[bold cyan]Initializing Secure Remote Connection...[/bold cyan]")
@@ -73,8 +69,12 @@ def remote_share(
    t = threading.Thread(target=start_server, args=(host, port), daemon=True)
    t.start()
 
-   console.print(f"[bold green]Server running, starting tunnel...[/bold green]")
-   tunnel(port)  # blocks here, keeps process alive
+   console.print(f"[bold cyan]Server started, creating remote tunnel...[/bold cyan]")
+
+   def on_url(url):
+      console.print(f"\n[bold green]Remote Tunnel created at {url}[/bold green]")
+
+   start_tunnel(port, on_url=on_url)   # blocks here, keeps process alive
 
 
 if __name__ == "__main__":
